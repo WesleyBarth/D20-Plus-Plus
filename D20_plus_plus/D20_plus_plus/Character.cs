bc4efa1dbaa1e344ca14aab8cca74646c7be56a5
WesleyBarth <wesbarth@gmail.com>
2017-09-30 12:14:25 -0600

    rearrange file structure

---
+++ D20_plus_plus/D20_plus_plus/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D20_plus_plus
+{
+    class Character
+    {
+
+    }
+
+    struct _ability
+    {
+        private string _name;
+        private int _score;
+        private int _modifier;
+
+        _ability(string name)
+        {
+            _name = name;
+            _score = 0;
+            _modifier = 0;
+        }
+    }
+
+    struct _race
+    {
+
+    }
+
+    struct _class
+    {
+
+    }
+}
